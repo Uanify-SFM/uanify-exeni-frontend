b7fbb64957ee9f28081a9cc4441a6b53e7a17556
ricardogf <ricardogf396@gmail.com>
2024-04-04 04:21:59 -0600

    add: record page

--- src/pages/patient-record-detail/index.tsx
+++ src/pages/patient-record-detail/index.tsx
@@ -1,5 +1,21 @@
+import PathContainer from "../../components/path-container";
+import DetailTable from "../../components/table/DetailTable";
+
 function PatientRecordDetail() {
-  return <div>PatientRecordDetail</div>;
+  return (
+    <div className="flex gap-x-4 h-full">
+      <div className="bg-white border rounded-2xl w-[70vw]">
+        <PathContainer
+          idPatient="1234"
+          patientName="Ricardo González Flores"
+          idRecord="SVHFA134"
+          recordName="SVHFA134"
+        />
+        <DetailTable />
+      </div>
+      <div className="bg-white border rounded-2xl w-[30vw]"></div>
+    </div>
+  );
 }
 
 export default PatientRecordDetail;
--- src/pages/patient-record/index.tsx
+++ src/pages/patient-record/index.tsx
@@ -1,5 +1,16 @@
+import PathContainer from "../../components/path-container";
+import RecordsTable from "../../components/table/RecordsTable";
+
 function PatientRecord() {
-  return <div>PatientRecord</div>;
+  return (
+    <div className="flex gap-x-4 h-full">
+      <div className="bg-white border rounded-2xl w-[70vw]">
+        <PathContainer idPatient="1234" patientName="Ricardo González Flores" />
+        <RecordsTable />
+      </div>
+      <div className="bg-white border rounded-2xl w-[30vw]"></div>
+    </div>
+  );
 }
 
 export default PatientRecord;
